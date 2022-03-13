92c7d911db9639d80cf6064cf1bac4c9fbb4cf4f
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-13 20:08:17 +0000

    Update Footer with source code link

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
+import { Header } from 'semantic-ui-react';
+import styles from 'styles/Footer.module.css';
 
 const Footer = () => {
     console.log('footer');
 
-    return <div>Footer</div>;
+    return (
+        <div className={styles.container}>
+            <Image src='/github.svg' width={25} height={25} alt='' />
+            &nbsp; Source code&nbsp;
+            <a
+                href='https://github.com/munjyong/nextjs-discord-dashboard'
+                target='_blank'
+                rel='noreferrer'
+                className={styles.link}
+            >
+                here
+            </a>
+        </div>
+    );
 };
 
 export default Footer;
