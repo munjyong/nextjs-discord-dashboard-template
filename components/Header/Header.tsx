448a16f676d5962d5b8ad3cd945c974931807488
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-13 20:08:25 +0000

    Update Header items

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Container, Menu } from 'semantic-ui-react';
+import styles from 'styles/Header.module.css';
 
 const Header = () => {
     console.log('header');
 
     return (
         <Container>
-            <Menu stackable>
+            <Menu stackable className={styles.menu}>
                 <Menu.Item>
                     <img src='/discord.svg' />
                 </Menu.Item>
-                <Menu.Item>Features</Menu.Item>
-                <Menu.Item>Testimonials</Menu.Item>
-                <Menu.Item>Sign-in</Menu.Item>
+                <Menu.Item className={styles.menuItem}>Home</Menu.Item>
+                <Menu.Item className={styles.menuItem}>Servers</Menu.Item>
+                <Menu.Item className={styles.menuItem}>Profile</Menu.Item>
             </Menu>
         </Container>
     );
