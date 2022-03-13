3381ed683f2d1398903743ef5420c7d5f8570159
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-13 20:08:44 +0000

    Remove log

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,21 +1,17 @@
+import React, { ReactChild } from 'react';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
-import React, { ReactChild } from 'react';
 
 interface Props {
     children: ReactChild;
 }
 
-const Layout = ({ children }: Props) => {
-    console.log('Layout');
-
-    return (
-        <>
-            <Header />
-            {children}
-            <Footer />
-        </>
-    );
-};
+const Layout = ({ children }: Props) => (
+    <>
+        <Header />
+        {children}
+        <Footer />
+    </>
+);
 
 export default Layout;
