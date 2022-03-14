42476e674069179abf4faab2b0fde5fb145b16fd
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-14 23:34:13 +0000

    Add linking to menu items

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { Container, Menu } from 'semantic-ui-react';
 import styles from 'styles/Header.module.css';
@@ -11,9 +12,15 @@ const Header = () => {
                 <Menu.Item>
                     <img src='/discord.svg' />
                 </Menu.Item>
-                <Menu.Item className={styles.menuItem}>Home</Menu.Item>
-                <Menu.Item className={styles.menuItem}>Servers</Menu.Item>
-                <Menu.Item className={styles.menuItem}>Profile</Menu.Item>
+                <Menu.Item className={styles.menuItem}>
+                    <Link href='/'>Home</Link>
+                </Menu.Item>
+                <Menu.Item className={styles.menuItem}>
+                    <Link href='/servers'>Servers</Link>
+                </Menu.Item>
+                <Menu.Item className={styles.menuItem}>
+                    <Link href='/profile'>Profile</Link>
+                </Menu.Item>
             </Menu>
         </Container>
     );
