42d26a07e8491f5577b92136cdb15da8ff360da1
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-26 16:32:16 +0000

    Add session provider wrapper to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import Layout from 'components/Layout';
+import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import 'semantic-ui-css/semantic.min.css';
 import 'styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-    <Layout>
-        <Component
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            {...pageProps}
-        />
-    </Layout>
+    <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <Layout>
+            <Component
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...pageProps}
+            />
+        </Layout>
+    </SessionProvider>
 );
 
 export default MyApp;
