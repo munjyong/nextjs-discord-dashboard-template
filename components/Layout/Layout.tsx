a8d432e718e8df026ea6eff0e3ac570953cbfb76
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-26 16:32:03 +0000

    Add Head to Layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactChild } from 'react';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
+import Head from 'next/head';
 
 interface Props {
     children: ReactChild;
@@ -8,6 +9,11 @@ interface Props {
 
 const Layout = ({ children }: Props) => (
     <>
+        <Head>
+            <title>NextCord</title>
+            <meta name='description' content='Made with NextJS' />
+            <link rel='icon' href='/discord.svg' />
+        </Head>
         <Header />
         {children}
         <Footer />
