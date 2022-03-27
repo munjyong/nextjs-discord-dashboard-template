eee7daa6c0f2ace53248e2addab46ead0e6a361b
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-27 21:01:53 +0100

    Add UserProfile component

---
+++ components/UserProfile/UserProfile.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+import styles from 'styles/UserProfile.module.css';
+
+const UserProfile = () => {
+    console.log('profile');
+
+    return (
+        <main className={styles.main}>
+            <h1>My profile:</h1>
+            <Image
+                src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+                alt='Profile image'
+                width='100%'
+                height='100%'
+                className={styles.avatar}
+            />
+            <Card className={styles.card}>
+                <Card.Content>
+                    <Card.Header>Matthew</Card.Header>
+                    <Card.Meta>
+                        <span className='date'>Joined in 2015</span>
+                    </Card.Meta>
+                    <Card.Description>
+                        Matthew is a musician living in Nashville.
+                    </Card.Description>
+                </Card.Content>
+            </Card>
+        </main>
+    );
+};
+
+export default UserProfile;
