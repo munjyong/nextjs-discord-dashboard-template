b31d29ab0b33d8100ff264437c517980f1ed7b3b
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-27 02:01:16 +0100

    Update dynamic auth route with callbacks to pass access token to client

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,4 +11,20 @@ export default NextAuth({
             },
         }),
     ],
+    callbacks: {
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+                // eslint-disable-next-line no-param-reassign
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({ session, token, user }) {
+            // Send properties to the client, like an access_token from a provider.
+            // eslint-disable-next-line no-param-reassign
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
 });
