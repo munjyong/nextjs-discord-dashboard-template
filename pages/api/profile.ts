9d89ae85fe0cd4a66ce3f246b95ef657c7b83b6a
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-29 22:34:52 +0100

    Update profile endpoint to fetch user data

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -5,14 +5,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getSession({ req });
 
-    if (session) {
-        res.send({
-            content:
-                'This is protected content. You can access this content because you are signed in.',
-        });
-    } else {
-        res.send({
-            error: 'You must be signed in to view the protected content on this page.',
-        });
+    try {
+        if (session) {
+            const response = await await fetch(
+                'https://discord.com/api/users/@me',
+                {
+                    headers: {
+                        authorization: `Bearer ${session.accessToken}`,
+                    },
+                }
+            );
+
+            const data = await response.json();
+            console.log('🚀 ~ data', data);
+
+            res.statusCode = 200;
+            res.send(data);
+            res.end();
+        }
+    } catch (error) {
+        res.json(error);
+        res.status(405).end();
     }
 };
