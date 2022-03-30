c392db4f51eb7f1655159c681b6d8b089e9e19a5
Mun Jun Yong <40276401+munjyong@users.noreply.github.com>
2022-03-30 21:45:25 +0100

    Remove log

--- components/UserProfile/UserProfile.tsx
+++ components/UserProfile/UserProfile.tsx
@@ -3,32 +3,28 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 import styles from 'styles/UserProfile.module.css';
 
-const UserProfile = () => {
-    console.log('profile');
-
-    return (
-        <main className={styles.main}>
-            <h1>My profile:</h1>
-            <Image
-                src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
-                alt='Profile image'
-                width='100%'
-                height='100%'
-                className={styles.avatar}
-            />
-            <Card className={styles.card}>
-                <Card.Content>
-                    <Card.Header>Matthew</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>Joined in 2015</span>
-                    </Card.Meta>
-                    <Card.Description>
-                        Matthew is a musician living in Nashville.
-                    </Card.Description>
-                </Card.Content>
-            </Card>
-        </main>
-    );
-};
+const UserProfile = () => (
+    <main className={styles.main}>
+        <h1>My profile:</h1>
+        <Image
+            src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+            alt='Profile image'
+            width='100%'
+            height='100%'
+            className={styles.avatar}
+        />
+        <Card className={styles.card}>
+            <Card.Content>
+                <Card.Header>Matthew</Card.Header>
+                <Card.Meta>
+                    <span className='date'>Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>
+                    Matthew is a musician living in Nashville.
+                </Card.Description>
+            </Card.Content>
+        </Card>
+    </main>
+);
 
 export default UserProfile;
